5e6753a3ad53f4b107e30a4f40e0487fd090efda
marava2 <30926772+marava2@users.noreply.github.com>
2017-08-14 22:57:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,4 +5,5 @@ element.innerHTML = 'new Value';
 
 var img = document.getElementById('madi');
 img.onClick = function(){
-    img.style.marginLeft = '100px';};
+    img.style.marginLeft = '100px';
+    };
