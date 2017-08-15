2e26f77bc3e18f3b9f63116b5a20c1ece0b5b339
marava2 <30926772+marava2@users.noreply.github.com>
2017-08-15 15:47:47 +0530

    [imad-console] Updates ui/mainAPI.js

--- ui/mainAPI.js
+++ ui/mainAPI.js
@@ -1,14 +1,3 @@
-var button = document.getElementById('counter');
-var counter = 0;
- button.onclick = function(){
-// make a req to counter
-//capture response and store ina variable
-//render variable to correct span
-counter = counter+1;
- var span = document.getElementById('count');
-span.innerHTML = counter.toString();
-};
-
 console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'new Value';
