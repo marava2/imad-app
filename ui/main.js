ca9d745d5905ff77baccf2ab43b69fcc2dc975a9
marava2 <30926772+marava2@users.noreply.github.com>
2017-08-15 15:34:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,10 +12,8 @@ var img = document.getElementById('madi');
       var counter = 0;
           button.onclick = function(){
         // make a req to counter
-        
         //capture response and store ina variable
-        
-        //render variable to correct span
+                //render variable to correct span
         counter = counter+1;
         var span = document.getElementById('count');
         span.innerHTML = counter.toString();
