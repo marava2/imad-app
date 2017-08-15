c3ffc0422da200b21db1b5ad4472eeef56c5b22b
marava2 <30926772+marava2@users.noreply.github.com>
2017-08-15 16:40:16 +0530

    [imad-console] Updates ui/mainAPI.js

--- ui/mainAPI.js
+++ ui/mainAPI.js
@@ -6,7 +6,7 @@ var request = new XMLHttpRequest();
 request.onreadystatechange= function(){
     if(request.readyState===XMLHttpRequest.DONE){
         if(request.status===200){
-        var counter = response.requestText;
+        var counter = request.responseText;
         var span = document.getElementById('count');
         span.innerHTML = counter.toString();
         }
