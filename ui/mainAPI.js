c45797128ce7a1829873749f787eadcc8f65836b
marava2 <30926772+marava2@users.noreply.github.com>
2017-08-15 16:17:12 +0530

    [imad-console] Updates ui/mainAPI.js

--- ui/mainAPI.js
+++ ui/mainAPI.js
@@ -1,11 +1,25 @@
 var button = document.getElementById('counter');
-var counter = 0;
-
  button.onclick = function(){
+var request = new XMLHttpRequest();
+request.onreadystatechange= function(){
+    if(request.readyState===XMLHttpRequest.DONE){
+        if(request.status===200){
+        var counter = response.requestText;
+        var span = document.getElementById('count');
+        span.innerHTML = counter.toString();
+        }
+    }
+};
+};
+//Beow code for incrementing button function manually where using counter form server.js as webAPI now
+//var button = document.getElementById('counter');
+//var counter = 0;
+// button.onclick = function(){
+//var request = new XMLHttpRequest();
 // make a req to counter
 //capture response and store ina variable
 //render variable to correct span
-counter = counter+1;
- var span = document.getElementById('count');
-span.innerHTML = counter.toString();
-};
+//counter = counter+1;
+// var span = document.getElementById('count');
+//span.innerHTML = counter.toString();
+//};
