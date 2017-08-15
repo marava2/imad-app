1cb19e2ab74bbb56fe8be9ca21ebc98cf77d9400
marava2 <30926772+marava2@users.noreply.github.com>
2017-08-15 15:49:28 +0530

    [imad-console] Updates ui/mainAPI.js

--- ui/mainAPI.js
+++ ui/mainAPI.js
@@ -1,6 +1,11 @@
-console.log('Loaded!');
-var element = document.getElementById('main-text');
-element.innerHTML = 'new Value';
-var img = document.getElementById('madi');
-img.onclick = function () {
-img.style.marginLeft = '100px'; };
+var button = document.getElementById('counter');
+var counter = 0;
+
+ button.onclick = function(){
+// make a req to counter
+//capture response and store ina variable
+//render variable to correct span
+counter = counter+1;
+ var span = document.getElementById('count');
+span.innerHTML = counter.toString();
+};
