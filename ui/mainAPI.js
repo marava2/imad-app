b2b2b489209db4714c03699d9ab5c10159c92e58
marava2 <30926772+marava2@users.noreply.github.com>
2017-08-15 16:37:02 +0530

    [imad-console] Updates ui/mainAPI.js

--- ui/mainAPI.js
+++ ui/mainAPI.js
@@ -1,10 +1,19 @@
 var button = document.getElementById('counter');
-var counter = 0;
  button.onclick = function(){
-// make a req to counter
-//capture response and store ina variable
-//render variable to correct span
-counter = counter+1;
- var span = document.getElementById('count');
-span.innerHTML = counter.toString();
+     // create a request object 
+     // below is actually  done when we receive response from the request
+var request = new XMLHttpRequest();
+request.onreadystatechange= function(){
+    if(request.readyState===XMLHttpRequest.DONE){
+        if(request.status===200){
+        var counter = response.requestText;
+        var span = document.getElementById('count');
+        span.innerHTML = counter.toString();
+        }
+    }
+    // Make a request
+    //Below requests the counter 
+};
+    request.open('GET','http://madhusudhanarava9.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
