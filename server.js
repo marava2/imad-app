d45760177962a71cd857e0b9e7d296e3704e61d5
marava2 <30926772+marava2@users.noreply.github.com>
2017-08-19 12:01:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var Pool = require('pg').Pool; // DB CONNCECTION
+var config = {
+    user : 'madhusudhanarava9',
+    database : 'madhusudhanarava9',
+    port : '5432',
+    password :process.env.DB=PASSWORD
+};  // DB CONNCECTION
+
 var articleOne = {
 title: 'Article one | madhusudhan',
 heading : 'Heading 1',
@@ -26,6 +34,15 @@ function createTemplate(data){
 }
 
 var counter = 0;
+
+var pool = new Pool(config);// DB CONNCECTION
+app.get('/test-db', function(req,res){
+pool.query('select * from test', function(err, result){
+    if(err){res.status(500).send(err,toString());}
+    else {res.send(JSON.stringify(result));}
+});
+});// DB CONNCECTION
+
 app.get('/counter', function(req,res){
     counter = counter + 1; 
     res.send(counter.toString());
