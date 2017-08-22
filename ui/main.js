28f4ef7f0414c6ab8903b4ae142c2d6f1663e112
marava2 <30926772+marava2@users.noreply.github.com>
2017-08-22 10:49:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,9 @@ element.innerHTML = 'new Value';
 var img = document.getElementById('madi');
 img.onclick = function () {
 img.style.marginLeft = '100px'; };
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+console.log(username);
+console.log(password);
+ request.open('POST','http://madhusudhanarava9.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
