15f603c87a0fdd40388012e7467ed38a265ec0f6
marava2 <30926772+marava2@users.noreply.github.com>
2017-08-22 10:55:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,13 @@
+var submit = document.getElementById('submitbtn');
+ submit.onclick = function(){
+     // create a request object 
+     // below is actually  done when we receive response from the request
+var request = new XMLHttpRequest();
+request.onreadystatechange= function(){
+    if(request.readyState===XMLHttpRequest.DONE){
+         }
+}
+};
 console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'new Value';
