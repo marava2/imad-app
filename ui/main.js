40172aaab6a84943105d3ce84d2bc455d5665127
marava2 <30926772+marava2@users.noreply.github.com>
2017-08-22 11:10:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,11 @@
+console.log('Loaded!');
+var element = document.getElementById('main-text');
+element.innerHTML = 'new Value';
+var img = document.getElementById('madi');
+img.onclick = function () {
+img.style.marginLeft = '100px'; };
+
+// entire code below is for authenticating the user
 var submit = document.getElementById('submitbtn');
  submit.onclick = function(){
      // create a request object 
@@ -17,12 +25,6 @@ request.onreadystatechange= function(){
         }
 };
 };
-console.log('Loaded!');
-var element = document.getElementById('main-text');
-element.innerHTML = 'new Value';
-var img = document.getElementById('madi');
-img.onclick = function () {
-img.style.marginLeft = '100px'; };
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 console.log(username);
