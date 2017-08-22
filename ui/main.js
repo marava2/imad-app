553b6fd39f3e2038734af396c0825ef207cc8e6c
marava2 <30926772+marava2@users.noreply.github.com>
2017-08-22 10:45:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
-//var element = document.getElementById('main-text');
-//element.innerHTML = 'new Value';
+var element = document.getElementById('main-text');
+element.innerHTML = 'new Value';
 var img = document.getElementById('madi');
 img.onclick = function () {
 img.style.marginLeft = '100px'; };
