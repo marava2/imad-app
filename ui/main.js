9562aafe2fd6207d1d60529e829db4baeeda4916
marava2 <30926772+marava2@users.noreply.github.com>
2017-08-25 22:12:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,6 +10,7 @@ var submit = document.getElementById('submitbtn');
  submit.onclick = function(){
      // create a request object 
      // below is actually  done when we receive response from the request
+     alert ('Hi, am serverside scripting');
 var request = new XMLHttpRequest();
 request.onreadystatechange= function(){
     if(request.readyState===XMLHttpRequest.DONE){
