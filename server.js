51208c4aa3cc5f10f5f930f14c7ff46193ad5f01
marava2 <30926772+marava2@users.noreply.github.com>
2017-08-26 17:45:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.post('/login', function(req,res){
     var username = req.body.username;
     var password = req.body.password;
   pool.query('select * from  "users" where username = $1',[username], function(err, result){
-    if(err){res.status(500).send(err,toString());}
+    if(err){res.status(500).send(err.toString());}
     else {
         if(result.rows.length ===0 ){res.send(403).send('username/password is invalid');}
        else {
