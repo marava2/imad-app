a08902d959f747aafc1a01f03a13362fd5d312a5
marava2 <30926772+marava2@users.noreply.github.com>
2017-08-26 12:28:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ function hash (input, salt)
 {
 var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
 return hashed.toString('hex');
-//return ["pbkdf2","10000",salt,hashed.toString('hex')].join('$');
+return ["pbkdf2","10000",salt,hashed.toString('hex')].join('$');
 } //  Hash Function
 
 app.get('/hash/:input', function(req,res){
