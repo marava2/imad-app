f4df435f01baefe791510ef0c89735d35b03ac9f
marava2 <30926772+marava2@users.noreply.github.com>
2017-08-26 09:06:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,5 +30,5 @@ console.log(username);
 console.log(password);
  request.open('POST','http://madhusudhanarava9.imad.hasura-app.io/login',true);
  request.setRequestHeader('Content-Type','application/json');
- request.send(JSON.stringify({username : username, password: password }));
+ request.send(JSON.stringify({username: username, password: password}));
  };
