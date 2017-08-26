0565aa8737e86e272b3e91102785815a1e340a93
marava2 <30926772+marava2@users.noreply.github.com>
2017-08-26 12:16:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ Pool.query('INSERT INTO "user" (username, password) VALUES($1,$2)',[username, db
 app.post('/login', function(req,res){
     var username = req.body.username;
     var password = req.body.password;
-pool.query('select * from  "user" where username = $1',[username], function(err, result){
+Pool.query('select * from  "user" where username = $1',[username], function(err, result){
     if(err){res.status(500).send(err,toString());}
     else {
         if(result.rows.length ===0 ){res.send(403).send('username/password is invalid');}
